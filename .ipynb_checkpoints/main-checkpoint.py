020ea7deefc92514a41be7caea8b5710555bbdcd
AndrewLondon <andrewmart@mail.com>
2024-06-29 19:14:58 +0100

    AI bot

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,11 @@
+from numpy import np
+from pandas import pd
+
+#Exercise 9
+
+#Linear Regression
+
+data = pd.read_csv('Bitcoin Historical Price Data.csv')
+data.keys()
+
+print (data)
---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+# Exercise 9
+
+# Linear Regression
+
+data = pd.read_csv('Bitcoin Historical Price Data.csv')
+print(data.keys())
+print(data)
